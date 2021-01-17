e39b53d38e19daba68864e53417f3ccb4327a93b
kaustu88 <kaustubha.v88@gmail.com>
2021-01-17 16:15:27 +0530

    second commit

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -4,9 +4,7 @@ from FlaskTemplate import app
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    # TODO: Create a query parameter so as to add our name to the URL like 
-    # https://first-project.azurewebsites.net/name=Grace
+   
     name = request.args.get('name')
 
     return render_template(
